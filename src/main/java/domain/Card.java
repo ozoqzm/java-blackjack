458a8da53b7a27afddb1be646b5b77db2893152b
sojeong <spah7839@naver.com>
2024-07-21 06:19:45 +0900

    feat: Card 도메인

--- src/main/java/domain/Card.java
+++ src/main/java/domain/Card.java
@@ -1,4 +1,20 @@
 package domain;
 
 public class Card {
+    private Denomination denomination;
+
+    // 카드 생성자
+    public Card(Denomination denomination) {
+        this.denomination = denomination;
+    }
+
+    public int getValue() {
+        return denomination.getScore();
+    }
+
+    // Ace 인지 여부
+    public boolean isAce() {
+        return denomination.isAce();
+    }
+
 }
